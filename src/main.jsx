f9fa61e94e800ef251b5452db86566bc5f0964a4
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-16 00:56:59 -0300

    atualizando commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ContextProviderCarrinho from "./context/CarrinhoContext.jsx";
 import ContextProviderLogin from "./context/LoginContext.jsx";
 import "./index.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <HashRouter>
+    <BrowserRouter>
         <ContextProviderCarrinho>
             <ContextProviderLogin>
                 <App />
             </ContextProviderLogin>
         </ContextProviderCarrinho>
-    </HashRouter>
+    </BrowserRouter>
 );
